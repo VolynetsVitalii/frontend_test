06eb3c62a0fe7e0b2303ef0b6189230bf874d7be
VolynetsVitalii <82473613+VolynetsVitalii@users.noreply.github.com>
2023-02-14 11:58:08 +0100

    add test for N = 1, N = 4, N = 10, N = 0

--- Tasks/create_array/createArray.js
+++ Tasks/create_array/createArray.js
@@ -1,6 +1,11 @@
 // Declare a function
 function createArray(N) {
     let arrNumbers = [];
+    console.log(N);
+    for (let i = 1; i <= N; i++) {
+        console.log(i);
+        arrNumbers.push(i);
+    }
     return arrNumbers;
 }
 // Exporting a Function
--- Tasks/create_array/createArray.test.js
+++ Tasks/create_array/createArray.test.js
@@ -9,4 +9,20 @@ describe('Create Array', () => {
     test('Function createArray should return an array', () => {
         expect(Array.isArray(createArray())).toBe(true);
     });
+    test('Function createArray should return [1] if N = 1', () => {
+        const N = 1;
+        expect(createArray(N)).toEqual([1]);
+    });
+    test('Function createArray should return [1,2,3,4] if N = 4', () => {
+        const N = 4;
+        expect(createArray(N)).toEqual([1, 2, 3, 4]);
+    });
+    test('Function createArray should return [1,2,3,4,5,6,7,8,9,10] if N = 10', () => {
+        const N = 10;
+        expect(createArray(N)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
+    test('Function createArray should return an empty array if N = 0', () => {
+        const N = 0;
+        expect(createArray(N)).toEqual([]);
+    })
 });
